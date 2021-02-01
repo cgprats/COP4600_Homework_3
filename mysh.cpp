9c247a96432dbb9de7b035d7bbaa7b1629cc151c
Christopher Prats <christopher@B450-1.ChrisPrats.lan>
2021-02-01 17:14:59 -0500

    Created Shell Class

--- mysh.cpp
+++ mysh.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
+#include <string>
+
+// The Shell Class
+class Shell {
+	private:
+		std::string _currentDirectory = "";
+	public:
+		Shell(std::string);
+		void SetCurrentDirectory(std::string);
+		std::string GetCurrentDirectory();
+};
+
+// The Main Function
 int main() {
+	return 0;
+}
+
+// Shell Constructor
+
+// Set the Working Directory
+void Shell::SetCurrentDirectory(std::string currentDirectory) {
+	_currentDirectory = currentDirectory;
+}
+
+// Get the Working Directory
+std::string Shell::GetCurrentDirectory() {
+	return _currentDirectory;
 }
